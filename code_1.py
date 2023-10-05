875a35f765700a030e41d95bcdedc272fe47069d
Lora <143578889+Lora207@users.noreply.github.com>
2023-10-05 08:10:39 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+}
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+    }
+            System.out.println("Random Number: " + item);
