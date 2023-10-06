f67f171ba50bf72fd4e827368a41799192053fa5
Lora <143578889+Lora207@users.noreply.github.com>
2023-10-06 14:22:37 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,13 @@
-}
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
 
-        for (int item : data) {
-    public static void main(String[] args) {
-public class RandomDataGenerator {
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+    data = generate_random_data()
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        }
-    }
-            System.out.println("Random Number: " + item);
+import random
+
+    main()
