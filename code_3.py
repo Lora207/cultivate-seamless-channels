1de532227da2647ae8056b5328b67ca63bf20b20
Lora <143578889+Lora207@users.noreply.github.com>
2023-10-12 21:40:25 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,17 @@
+    return data;
+    data.forEach(item => {
+    });
+function generateRandomData() {
+main();
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = [];
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+}
+        console.log("Random Number: " + item);
 
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-    return data
-    data = generate_random_data()
 
-def generate_random_data():
-    for item in data:
 
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-def main():
-
-    main()
+}
